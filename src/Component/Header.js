35469dfbefd9220b5fc509553692cc0986b0e793
munir79 <jakirhossainmunir79@gmail.com>
2023-09-27 16:04:04 +0600

    auth setup

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Context/UserContext';
 
 const Header = () => {
+    const {user}=useContext(AuthContext);
     return (
         <div>
             <Link to='/login'> Log in </Link>
             <Link to='/registar'> Registar </Link>
+            <p> { <span>wellcome {user?.displayName} </span> } </p>
+            
         </div>
     );
 };
--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,13 +1,18 @@
 import React, { createContext } from 'react';
-const AuthContext=createContext();
+import {getAuth} from 'firebase/auth'
+import app from '../Firebase/firebase.init';
+ export const AuthContext=createContext();
 
 
-const UserContext = ({children}) => {
-    const AuthInfo={};
+const auth =getAuth(app);
+const UserContext = ({children}) => { 
+    
+    const user={displayName:'munir'}
+    const AuthInfo={user};
     return (
         <div>
             <AuthContext.Provider value={AuthInfo}>
-
+                {children}
             </AuthContext.Provider>
         </div>
     );
